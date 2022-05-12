91700022bac892ca7bab8878f87edd57686f8548
Cory <cameyer182@gmail.com>
2022-05-11 20:32:22 -0400

    commit tree cadc8c574b51f3e12442e6dc17143c2ca71e8992

--- src/index.js
+++ src/index.js
@@ -1,15 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import Home from "./Home";
+import About from "./About";
+import Login from "./Login";
+import Navbar from "./Navbar";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-const Home = () => {
+const Messages = () => <h1>Messages go here!</h1>
+
+const Signup = () => {
   return (
-    <div>
-      <h1>Home!</h1>
-    </div>
+  <div>
+    <form>
+      <div>
+        <input type="text" name="username" placeholder='Username' />
+        <label htmlFor='username'>Username</label>
+      </div>
+      <div>
+        <input type="password" name='password' placeholder='Password' />
+        <label htmlFor='password'>Password</label>
+      </div>
+      <div>
+        <input type='email' name='email' placeholder='Email' />
+        <label htmlFor='email'>Email</label>
+      </div>
+    </form>
+  </div>
   );
 };
 
 ReactDOM.render(
-  <Home />,
+  <Router>
+    <div>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/messages" component={Messages} />
+      <Route exact path="/signup" component={Signup} />
+    </div>
+  </Router>,
   document.getElementById('root')
 );
